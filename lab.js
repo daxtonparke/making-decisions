da216e7da108af4318653f0b11488a00cef1c9f8
daxtonparke <daxtonparke@gmail.com>
2021-11-10 16:48:34 -0700

    q10

--- lab.js
+++ lab.js
@@ -129,13 +129,17 @@ while(score<passingScore){
 
 */
 
-//CODE HERE
+changeMyMind= true
+if(changeMyMind=true){
+  changeMyMind=false
+} else changeMyMind=true
 
 ////////// PROBLEM 10 //////////
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
 
-//CODE HERE
+changeMyMind= !changeMyMind
 
+console.log(changeMyMind)
 
 
 ////////// ADVANCED PROBLEMS //////////
